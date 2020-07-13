b82e603a522f0eb69ae8823b79a9ff8ecf96896e
Dani0210 <alejandrad087@gmail.com>
2020-07-12 19:46:01 -0500

    Tarjeta Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import validator from './validator.js';
+let Compra = document.getElementById('Comprar');
+Compra.addEventListener('click' , DarComprar);
+let Compra1 = document.getElementById('Comprar1');
+Compra1.addEventListener('click' , DarComprar);
+function DarComprar()
+{
+  alert("Recuerda que solo aceptamos tarjeta de credito como medio de pago")
+  document.getElementById('vista0').style.display="none";
+  document.getElementById('vista2').style.display="none";
+  document.getElementById('vista1').style.display="block";
+}
 
-console.log(validator);
+ let valid=document.getElementById('validacion');
+valid.addEventListener('click',ContinuaValidacion);
+function ContinuaValidacion()
+{ 
+  document.getElementById('vista1').style.display="none";
+  document.getElementById('vista2').style.display="block";
+}
+  
+
+let volver = document.getElementById('volver');
+volver.addEventListener('click' ,Regresar);
+
+function Regresar() 
+{ 
+  document.getElementById('vista0').style.display="none";
+  document.getElementById('vista2').style.display="none";
+document.getElementById('vista1').style.display="block";
+} 
+let resultado=document.getElementById('result');
+resultado.addEventListener('click',Validar);
+function Validar(){
+  document.getElementById('vista0').style.display="none";
+  document.getElementById('vista2').style.display="none";
+  document.getElementById('vista1').style.display="none";
+  document.getElementById('vista3').style.display='block';
+ /*  let vista3 = document.getElementById('vista3');
+  vista3.style.display="block"; */
+
+let numero = document.formtarjeta.number.value;
+let numMasky= validator.maskify(numero);
+let inputNombre=document.getElementById('inputNombre');
+let esValida= validator.isValid(numero);
+if (numero === '') {
+  alert ( 'Por favor complete los campos correspondientes para continuar')
+}else {
+   esValida= validator.isValid(numero);
+    
+}
+if(esValida){
+    numero = document.formtarjeta.number.value;
+    document.getElementById('vista 3').innerHTML=inputNombre.value +' '+ 'Tu tarjeta de credito'+ ' ' + numMasky +' '+ 'es valida' ;
+    
+}else{
+   numero = document.formtarjeta.number.value;
+   document.getElementById('vista3').innerHTML= inputNombre.value +' '+ 'Tu tarjeta de credito'+ ' ' + numMasky +' '+ 'es invalida ,por favor verifica los datos e intenta nuevamente';
+}
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
+   isValid:(creditCardNumber)=>{
+    let suma = 0;
+    let restar=0;
+    let inverso = Array.from(creditCardNumber)
+    inverso.reverse();
+  for(let i = 0 ;i < inverso.length ; i++){
+  let elemento = inverso[i];
+ 
+   if( i % 2 !== 0){
+       let multiplicar = elemento*2;
+       
+    if(multiplicar >=10)
+    { 
+       restar = multiplicar - 9;
+         suma =suma + restar;
+   
+      }else{ suma = suma + multiplicar; 
+}
+  }else{
+    suma=suma+parseInt(elemento);
+   
+  }
+  
+    }
+    if(suma%10==0){
+      //console.log(suma);
+        return true;
+    }else{
+      // console.log(suma); 
+      return false;
+      
 
-export default validator;
+    }
+    
+    },    
+    maskify:function mask (creditCardNumber) {
+      let newNumber = creditCardNumber.replace(/\d(?=\d{4})/g,"#");
+      return newNumber;      
+  }  
+ 
+};
+ export default validator;
