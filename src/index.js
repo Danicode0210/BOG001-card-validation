5d509df49051844f0d2aa7ee5805c8e28fc5713f
Dani0210 <alejandrad087@gmail.com>
2020-08-06 19:22:29 -0500

    cambio a sports

--- src/index.js
+++ src/index.js
@@ -1,60 +1,109 @@
 import validator from './validator.js';
-let Compra = document.getElementById('Comprar');
-Compra.addEventListener('click' , DarComprar);
-let Compra1 = document.getElementById('Comprar1');
-Compra1.addEventListener('click' , DarComprar);
-function DarComprar()
-{
-  alert("Recuerda que solo aceptamos tarjeta de credito como medio de pago")
-  document.getElementById('vista0').style.display="none";
-  document.getElementById('vista2').style.display="none";
-  document.getElementById('vista1').style.display="block";
+
+// Cambio de pantallas
+const buttonBuy = document.getElementById('btnBuy'); //Botón comprar
+const instrumentsPage = document.getElementById('instruments'); //pantalla 1
+const paymentPage = document.getElementById('payment'); //pantalla 2
+
+buttonBuy.addEventListener('click', showPayment);
+
+function showPayment() {
+  paymentPage.classList.remove('hidden');
+  instrumentsPage.classList.add('hidden');
 }
 
- let valid=document.getElementById('validacion');
-valid.addEventListener('click',ContinuaValidacion);
-function ContinuaValidacion()
-{ 
-  document.getElementById('vista1').style.display="none";
-  document.getElementById('vista2').style.display="block";
+// Alerta de no stock para botón "Agotado"
+const buttonDisabled = document.querySelector('.btnDisabled'); //Botón agotado, solo selecciona el primer elemento de la clase
+
+buttonDisabled.addEventListener('click', noStock);
+
+function noStock() {
+  alert('Muy pronto lo tendremos en stock');
 }
+
+// Menú desplegable en el select mes
+const form = document.getElementById('formCard'); //Formulario
+
+for (let i = 1; i <= 12; i++) {
+  let optionMonths = document.createElement('option');
+  optionMonths.value = i;
+  optionMonths.textContent = i;
+  form.selectMonth.appendChild(optionMonths);
+}
+
+// Menú desplegable en el select año
+const yearActual = new Date().getFullYear();
+
+for(let i = yearActual; i <= yearActual + 4; i++){
+  let optionYears = document.createElement('option');
+	optionYears.value = i;
+	optionYears.textContent = i;
+	form.selectYear.appendChild(optionYears);
+}
+
+// Reflejar Número en TC, no admitir letras en el campo
+const numberCreditCard = document.getElementById('dataNumber');
+const inputNumberTC = document.getElementById('inputNumberCard');
+
+inputNumberTC.addEventListener('keyup', e => {
+  let numberValue = e.target.value;//Guardar el valor del input para abajo generar método replace con expresiones regulares
+  inputNumberTC.value = numberValue.replace(/\D/g, '');// \D = Busca las letras y las reemplaza por string vacío
+  numberCreditCard.textContent = validator.maskify(inputNumberTC.value); //Enmascarar el número en TC 
+  if(numberValue == ''){
+    numberCreditCard.textContent = '#### #### #### ####';
+  }
+});
+
+// Validación tarjeta
+const errorValidation = document.getElementById('errorValidation')
+
+inputNumberTC.addEventListener('blur', function(){
+  const isValid = validator.isValid(inputNumberTC.value);
   
+  if (isValid == false) {
+    errorValidation.textContent = "El numero de tarjeta ingresado es inválido"
+  } else {
+    errorValidation.textContent = ""
+  }
+});
 
-let volver = document.getElementById('volver');
-volver.addEventListener('click' ,Regresar);
+// Reflejar Nombre en TC 
+const nameCreditCard = document.getElementById('dataName');
+const inputNameTC = document.getElementById('inputName');
 
-function Regresar() 
-{ 
-  document.getElementById('vista0').style.display="none";
-  document.getElementById('vista2').style.display="none";
-document.getElementById('vista1').style.display="block";
-} 
-let resultado=document.getElementById('result');
-resultado.addEventListener('click',Validar);
-function Validar(){
-  document.getElementById('vista0').style.display="none";
-  document.getElementById('vista2').style.display="none";
-  document.getElementById('vista1').style.display="none";
-  document.getElementById('vista3').style.display='block';
- /*  let vista3 = document.getElementById('vista3');
-  vista3.style.display="block"; */
+inputNameTC.addEventListener('keyup', e => {
+  let nameValue = e.target.value;
+  inputNameTC.value = nameValue.replace(/\d/g,'').replace(/\+/g,'');// \d= Dígitos \+= Caracteres reservados menos (-,],\) \W= Ni letra, ni dígito
+  // inputNameTC.value = nameValue.replace(/[^A-Za-z\s]/g,'');// ^Negated set
+  nameCreditCard.textContent = nameValue;
+  if (nameValue == ''){
+    nameCreditCard.textContent = 'Nombre titular';
+  } 
+});
 
-let numero = document.formtarjeta.number.value;
-let numMasky= validator.maskify(numero);
-let inputNombre=document.getElementById('inputNombre');
-let esValida= validator.isValid(numero);
-if (numero === '') {
-  alert ( 'Por favor complete los campos correspondientes para continuar')
-}else {
-   esValida= validator.isValid(numero);
-    
-}
-if(esValida){
-    numero = document.formtarjeta.number.value;
-    document.getElementById('vista 3').innerHTML=inputNombre.value +' '+ 'Tu tarjeta de credito'+ ' ' + numMasky +' '+ 'es valida' ;
-    
-}else{
-   numero = document.formtarjeta.number.value;
-   document.getElementById('vista3').innerHTML= inputNombre.value +' '+ 'Tu tarjeta de credito'+ ' ' + numMasky +' '+ 'es invalida ,por favor verifica los datos e intenta nuevamente';
-}
+// Reflejar Mes en TC
+const monthCreditCard = document.getElementById('month');
+const inputMonthTC = document.getElementById('selectMonth');
+
+inputMonthTC.addEventListener('change', e => monthCreditCard.textContent = e.target.value);
+
+// Reflejar año en TC
+const inputYearTC = document.getElementById('selectYear');
+const yearCreditCard = document.getElementById('year');
+
+inputYearTC.addEventListener('change', e => yearCreditCard.textContent = e.target.value.slice(2));
+
+// Mensaje para tarjeta válida
+const btnSubmit = document.getElementById('validDataCard');
+
+btnSubmit.addEventListener('click', showValid);
+
+function showValid() {
+  const isValid = validator.isValid(inputNumberTC.value);
+  
+  if (isValid == false) {
+    alert("Ingresa un número de tarjeta que sea válido.");
+  } else {
+    alert("Tu pago ha sido exitoso");
+  }
 }
--- src/validator.js
+++ src/validator.js
@@ -1,43 +1,54 @@
 const validator = {
-   isValid:(creditCardNumber)=>{
-    let suma = 0;
-    let restar=0;
-    let inverso = Array.from(creditCardNumber)
-    inverso.reverse();
-  for(let i = 0 ;i < inverso.length ; i++){
-  let elemento = inverso[i];
- 
-   if( i % 2 !== 0){
-       let multiplicar = elemento*2;
-       
-    if(multiplicar >=10)
-    { 
-       restar = multiplicar - 9;
-         suma =suma + restar;
-   
-      }else{ suma = suma + multiplicar; 
-}
-  }else{
-    suma=suma+parseInt(elemento);
-   
-  }
-  
-    }
-    if(suma%10==0){
-      //console.log(suma);
-        return true;
-    }else{
-      // console.log(suma); 
-      return false;
-      
 
-    }
+  isValid: function(cardNumber){
+    //Convertir el numero de la tdc en array y obtener el inverso
+    let algorithmArray = cardNumber.split('').reverse();
+//----------------------------- forEach ----------------------------------------------
+    algorithmArray.forEach((element, index) => {
+
+    //Obtener numeros en las posiciones par y multiplicarlos por 2
+      if (index & 1) {
+        element = element * 2
+      
+      //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split 
+        if (element >= 10) {
+          element = element.toString().split('');
+          
+        //Convertir los digitos en numeros y sumarlos entre si
+          element = parseInt(element[0]) + parseInt(element[1]);
+      }
+    } 
     
-    },    
-    maskify:function mask (creditCardNumber) {
-      let newNumber = creditCardNumber.replace(/\d(?=\d{4})/g,"#");
-      return newNumber;      
-  }  
- 
+    //Colocar lo numeros que quedan en cada posicion del array
+      algorithmArray[index] = parseInt(element);
+    });
+    //Sumar todos los elementos del array
+    let sum = algorithmArray.reduce(function(x,y){
+      return x+y;
+    });
+
+    //Declarar el residuo de la division entre 10
+    let residue = sum % 10;
+
+    //Si el residuo es igual a 0 la tarjeta es valida, sino, es invalida
+    if (residue == 0) {
+      return true;
+    } else {
+      return false;
+    }     
+  },
+
+  // ------------------------------ Enmascaramiento --------------------------------
+  maskify: function(cardNumber){
+    // Obtener longitud del numero ingresado
+    const TClength = cardNumber.length;
+
+    // Desde 0 hasta longitud menos 4 reemplazar los numeros por #
+    for (let i = 0; i < TClength -4 ; i++){
+      cardNumber = cardNumber.replace(/\w/,'#'); // \w=  Matches any word character (alphanumeric & underscore)
+    }    
+    return cardNumber;
+  }
 };
- export default validator;
+
+export default validator;
