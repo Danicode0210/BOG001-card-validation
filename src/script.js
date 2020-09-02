173e21b6c0c83f5938961b35494d2df1661e1229
Dani0210 <alejandrad087@gmail.com>
2020-09-02 17:13:30 -0500

    Tarjet Finish

--- src/script.js
+++ src/script.js
@@ -1,42 +1,26 @@
-/* let inputNumero=document.getElementById('inputNumero');
-inputNumero.addEventListener("keyup" , (e) =>{
-  let valorInput= e.target.value
-//eliminamos los espacios en blanco
-  inputNumero.value = valorInput.replace(/\s/g, '')
-  //Eliminar las letras 
-  .replace(/\D/g, '')
-  //Ponemos espacio cada cuatro numeros, trim elimina el ultimo espacio
-  .replace(/([0-9]{4})/g, '$1 ').trim();
-   
-})  */
+let inputNombre = document.getElementById("inputNombre");
+inputNombre.addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
 
-let inputNombre=document.getElementById('inputNombre');
-inputNombre.addEventListener("keyup" , (e) =>{
-  let valorInput= e.target.value
-
- inputNombre.value = valorInput.replace(/[0-9]/g, '');
- 
- })
+  inputNombre.value = valorInput.replace(/[0-9]/g, "");
+});
 
 //vamos a poner la opcion de Seleccion en Mes y Año de Expiración
 
-let selectMes= document.getElementById('selectMes');
-let selectYear= document.getElementById('selectYear');
+let selectMes = document.getElementById("selectMes");
+let selectYear = document.getElementById("selectYear");
 
-
-for(let i=1; i <= 12; i++)
-
-{ let opcion= document.createElement('option');
-  opcion.value= i;
-  opcion.innerText= i;
+for (let i = 1; i <= 12; i++) {
+  let opcion = document.createElement("option");
+  opcion.value = i;
+  opcion.innerText = i;
   selectMes.appendChild(opcion);
 }
-const yearActual= new Date().getFullYear();
+const yearActual = new Date().getFullYear();
 
-for(let i = yearActual; i<= yearActual + 10; i++)
-{
-  let opcion= document.createElement('option');
-  opcion.value= i;
-  opcion.innerText= i;
+for (let i = yearActual; i <= yearActual + 10; i++) {
+  let opcion = document.createElement("option");
+  opcion.value = i;
+  opcion.innerText = i;
   selectYear.appendChild(opcion);
 }
--- src/validator.js
+++ src/validator.js
@@ -1,30 +1,28 @@
 const validator = {
 
-  isValid: function(cardNumber){
+  isValid: function (cardNumber) {
     //Convertir el numero de la tdc en array y obtener el inverso
     let algorithmArray = cardNumber.split('').reverse();
-//----------------------------- forEach ----------------------------------------------
+    //--- forEach -----
     algorithmArray.forEach((element, index) => {
 
-    //Obtener numeros en las posiciones par y multiplicarlos por 2
+      //Obtener numeros en las posiciones par y multiplicarlos por 2
       if (index & 1) {
         element = element * 2
-      
-      //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split 
+
         if (element >= 10) {
           element = element.toString().split('');
-          
-        //Convertir los digitos en numeros y sumarlos entre si
+
+          //Convertir los digitos en numeros y sumarlos entre si
           element = parseInt(element[0]) + parseInt(element[1]);
+        }
       }
-    } 
-    
-    //Colocar lo numeros que quedan en cada posicion del array
+
       algorithmArray[index] = parseInt(element);
     });
     //Sumar todos los elementos del array
-    let sum = algorithmArray.reduce(function(x,y){
-      return x+y;
+    let sum = algorithmArray.reduce(function (x, y) {
+      return x + y;
     });
 
     //Declarar el residuo de la division entre 10
@@ -35,18 +33,17 @@ const validator = {
       return true;
     } else {
       return false;
-    }     
+    }
   },
 
-  // ------------------------------ Enmascaramiento --------------------------------
-  maskify: function(cardNumber){
+  maskify: function (cardNumber) {
     // Obtener longitud del numero ingresado
     const TClength = cardNumber.length;
 
     // Desde 0 hasta longitud menos 4 reemplazar los numeros por #
-    for (let i = 0; i < TClength -4 ; i++){
-      cardNumber = cardNumber.replace(/\w/,'#'); // \w=  Matches any word character (alphanumeric & underscore)
-    }    
+    for (let i = 0; i < TClength - 4; i++) {
+      cardNumber = cardNumber.replace(/\w/, '#'); // \w=  Matches any word character (alphanumeric & underscore)
+    }
     return cardNumber;
   }
 };
